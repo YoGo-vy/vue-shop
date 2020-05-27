9ab7403d1258efaea814ae8b28cdc73be8f476ec
YoGo <869442530@qq.com>
2020-05-27 16:23:34 +0800

    登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="sign_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="80px"
+        class="login_form"
+      >
+        <el-form-item prop="username" label="用户名">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password" label="密码">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetFrom()">重置</el-button>
+        </el-form>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // component 组件数据模型
+  data: function() {
+    return {
+      // 表单数据
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      // 验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            min: 2,
+            max: 6,
+            message: '用户名长度为2~6个字符',
+            trigger: 'change'
+          }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码长度为6~16位', trigger: 'change' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetFrom: function() {
+      // 重置form表单：restFields()
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        //  false：验证失败，直接return结束后面axios代码执行
+        if (!valid) return console.log('验证失败，阻止axios请求')
+        //  验证成功，发送axios请求
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // 获取axios返回数据，不能直接过去PromiseValue
+        // 异步函数方式 + 对象结构赋值
+        if (res.meta.status !== 200) {
+          // 登录失败，直接返回结束
+          return this.$message.error({ message: res.meta.msg, showClose: true })
+        }
+        this.$message.success(res.meta.msg)
+        // 登录成功：1.记录token用于识别登录状态；2.编程式导航跳转主页
+        sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 6px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .sign_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    border-radius: 50%;
+    box-shadow: 0 0 10px #ddd;
+    box-sizing: border-box;
+    padding: 10px;
+    img {
+      width: 100%;
+      background-color: #eee;
+      border-radius: 50%;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 10px;
+  width: 100%;
+  padding: 0 25px 15px 15px;
+  box-sizing: border-box;
+  .btns {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: flex-end;
+    align-items: center;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import axios from 'axios'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
+// 引入axios，配置默认axios参数，将axios挂载在Vue的原型对象上以全局使用
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+// 设置路由导航守卫--控制页面路由访问权限
+router.beforeEach((to, from, next) => {
+  // 如果访问login--放行；return结束
+  if (to.path === '/login') return next()
+  // 如果访问非login，判断token
+  const tokenStr = sessionStorage.getItem('token')
+  // 没有token，直接返回结束，并且强制跳转到login
+  if (!tokenStr) return next('/login')
+  // 有token
+  next()
+})
 export default router
